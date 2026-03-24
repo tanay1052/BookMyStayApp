db2db32f630221f2fe0eb0eb6b0f1162d6a42fd9
tanay1052 <tanaypatel2808@gmail.com>
2026-03-24 10:56:17 +0530

    UC2

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,16 +1,87 @@
+
+/**
+ * Use Case 2: Basic Room Types & Static Availability
+ * Book My Stay App - Version 2.1
+ */
+
+abstract class Room {
+
+    protected String roomType;
+    protected int beds;
+    protected double price;
+
+    public Room(String roomType, int beds, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.price = price;
+    }
+
+    public abstract void displayRoomDetails();
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 2000);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: ₹" + price);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 3500);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: ₹" + price);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 6000);
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Price per night: ₹" + price);
+    }
+}
+
 public class BookMyStayApp {
-    public class U1 {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            System.out.println("=================================");
-            System.out.println("        Book My Stay App");
-            System.out.println("   Hotel Booking Management");
-            System.out.println("=================================");
-            System.out.println("Version : 1.0");
+        System.out.println("Book My Stay App - Version 2.1");
+        System.out.println("--------------------------------");
 
-            System.out.println("Welcome to the Hotel Booking System!");
-            System.out.println("Application started successfully.");
-        }
+        Room single = new SingleRoom();
+        Room doub = new DoubleRoom();
+        Room suite = new SuiteRoom();
+
+        int singleAvailability = 10;
+        int doubleAvailability = 5;
+        int suiteAvailability = 2;
+
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleAvailability);
+        System.out.println();
+
+        doub.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleAvailability);
+        System.out.println();
+
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteAvailability);
     }
 }
