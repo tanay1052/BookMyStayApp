1e0b0e2887459921af2d91027d4841e62d8b4161
tanay1052 <tanaypatel2808@gmail.com>
2026-03-24 10:49:49 +0530

    UC1

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,2 +1,16 @@
 public class BookMyStayApp {
+    public class U1 {
+
+        public static void main(String[] args) {
+
+            System.out.println("=================================");
+            System.out.println("        Book My Stay App");
+            System.out.println("   Hotel Booking Management");
+            System.out.println("=================================");
+            System.out.println("Version : 1.0");
+
+            System.out.println("Welcome to the Hotel Booking System!");
+            System.out.println("Application started successfully.");
+        }
+    }
 }
